7865a7cac591145875ed721c2750ca6c2bffadfe
Verachad Wongsawangtham <verachad.w@taskworld.com>
2014-10-17 17:06:45 +0700

    First commit

---
+++ app/src/main/kotlin/poc/tw/realmkotlin/MyActivity.kt
@@ -0,0 +1,16 @@
+package poc.tw.realmkotlin
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by VerachadW on 10/17/2014 AD.
+ */
+
+class MyActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super<Activity>.onCreate(savedInstanceState)
+
+    }
+
+}
