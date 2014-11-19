49ce6ce96ec1418713a5ae898c43cc44c35712ad
Verachad Wongsawangtham <verachad.w@taskworld.com>
2014-11-19 14:07:21 +0700

    Done with create playlist service

--- app/src/main/kotlin/com/taskworld/android/restfulandroidkotlin/network/request/PostCreatePlayListSpiceRequest.kt
+++ app/src/main/kotlin/com/taskworld/android/restfulandroidkotlin/network/request/PostCreatePlayListSpiceRequest.kt
@@ -8,10 +8,17 @@ import com.taskworld.android.restfulandroidkotlin.model.PlayList
 /**
  * Created by VerachadW on 11/18/14.
  */
-class PostCreatePlayListSpiceRequest(val path: String, val body: Map<String, String>):
+class PostCreatePlayListSpiceRequest(val path: String, val entity: PlayList):
         RetrofitSpiceRequest<Map<String, String>, TheMovieDBAPI.ListAPI>(javaClass<Map<String, String>>(), javaClass<TheMovieDBAPI.ListAPI>()) {
 
     override fun loadDataFromNetwork(): Map<String, String>? {
-        return getService().createPlayList(path, body)
+        return getService().createPlayList(path, createRequestBody(entity))
+    }
+
+    fun createRequestBody(entity: PlayList): Map<String, String> {
+        val map = hashMapOf<String, String>()
+        map.put("name", entity.getName())
+        map.put("description", entity.getDescription())
+        return map
     }
 }
