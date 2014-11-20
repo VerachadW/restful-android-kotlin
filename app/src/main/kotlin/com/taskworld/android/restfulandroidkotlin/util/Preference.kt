d7ca0ab65ec3e78051bbd73e882a26f7b9e1da12
Verachad Wongsawangtham <verachad.w@taskworld.com>
2014-11-20 12:17:28 +0700

    Restrict Preference to call with() before access properties

--- app/src/main/kotlin/com/taskworld/android/restfulandroidkotlin/util/Preference.kt
+++ app/src/main/kotlin/com/taskworld/android/restfulandroidkotlin/util/Preference.kt
@@ -9,7 +9,7 @@ object Preference {
 
     private val NAME = "movie"
 
-    private var mSharedPreferences: SharedPreferences by Delegates.notNull()
+    private var mSharedPreferences: SharedPreferences?  = null
 
     //
     var username: String? by PreferenceDelegate()
@@ -24,20 +24,26 @@ object Preference {
         private var value: T = null
 
         override fun get(thisRef: Any?, desc: PropertyMetadata): T {
+
+            if (mSharedPreferences == null) throw NullPointerException("Preference.with() must be called before acessing properties")
+
             [suppress("unchecked_cast")]
             when (value) {
-                is String -> value = mSharedPreferences.getString(desc.name, null) as T
-                is Float -> value = mSharedPreferences.getFloat(desc.name, 0.0f) as T
-                is Int -> value = mSharedPreferences.getInt(desc.name, 0) as T
-                is Boolean -> value = mSharedPreferences.getBoolean(desc.name, false) as T
-                is Long -> value = mSharedPreferences.getLong(desc.name, 0L) as T
+                is String -> value = mSharedPreferences!!.getString(desc.name, null) as T
+                is Float -> value = mSharedPreferences!!.getFloat(desc.name, 0.0f) as T
+                is Int -> value = mSharedPreferences!!.getInt(desc.name, 0) as T
+                is Boolean -> value = mSharedPreferences!!.getBoolean(desc.name, false) as T
+                is Long -> value = mSharedPreferences!!.getLong(desc.name, 0L) as T
                 else -> throw IllegalArgumentException("${desc.name} variable type is not supported yet!!")
             }
             return value
         }
 
         override fun set(thisRef: Any?, desc: PropertyMetadata, value: T) {
-            val editor = mSharedPreferences.edit()
+
+            if (mSharedPreferences == null) throw NullPointerException("Preference.with() must be called before acessing properties")
+
+            val editor = mSharedPreferences!!.edit()
             when (value) {
                 is String -> editor.putString(desc.name, value)
                 is Float -> editor.putFloat(desc.name, value)
